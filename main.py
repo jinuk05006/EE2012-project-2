c91459b7b741dc4ddf6e6190490c1dbe0405943f
chewyongzhang <88772274+chewyongzhang@users.noreply.github.com>
2022-11-05 21:34:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,36 +14,41 @@ def plotting_equation():
         return (1/t)*np.exp(-(x/t))
     def the_cdf_eq(t, x):
         return 1-np.exp(-(1/t)*x)
+    def integrad_eq(eq, start, end):
+        return quad(eq, start, end )[0]
+    y_p=integrad_eq(lambda x:the_pdf_eq(3,x), 5, np.inf)
+    z_p=integrad_eq(lambda x:the_pdf_eq(4,x), 5, np.inf)
+    a_p=integrad_eq(lambda x:the_pdf_eq(5,x), 5, np.inf)
+    print(f"The p when lambda is 1/3 is {y_p}.\n")
+    print(f"The p when lambda is 1/4 is {z_p}.\n")
+    print(f"The p when lambda is 1/5 is {a_p}.\n")
     if function=="pdf":
         y = the_pdf_eq(3,x)
         z = the_pdf_eq(4,x)
         a = the_pdf_eq(5,x)
-       
+        #fig = plt.figure(figsize = (10, 5))
+        plt.plot(x, y, color='r', label=f"t=3 ; lambda=1/3 , p={y_p}")
+        plt.plot(x, z, color='g', label=f"t=4 ; lambda=1/4 , p={z_p}")
+        plt.plot(x, a, color="b", label=f"t=5 ; lambda=1/5 , p={a_p}")
+        plt.fill_between(x_fill,the_pdf_eq(3, x_fill),color="r", alpha=0.5)
+        plt.fill_between(x_fill,the_pdf_eq(4, x_fill),color="g", alpha=0.5)
+        plt.fill_between(x_fill,the_pdf_eq(5, x_fill),color="b", alpha=0.5)
+        plt.xlabel("x value")
+        plt.ylabel(f"{function}")
+        plt.title(f"{function} Graphs at different lambda values")
+
     if function=="cdf":
         y = the_cdf_eq(3, x)
         z = the_cdf_eq(4, x)
         a = the_cdf_eq(5, x)
-    print(integrad_eq(lambda x:the_pdf_eq(3,x), 5, np.inf))
-    fig = plt.figure(figsize = (10, 5))
-    # Plotting both the curves simultaneously
-    plt.plot(x, y, color='r', label='lambda=1/3')
-    plt.plot(x, z, color='g', label='lambda=1/4')
-    plt.plot(x, a, color="b", label="lambda=1/5")
-    
-    plt.fill_between(x_fill,the_pdf_eq(3, x_fill), alpha=0.5)
-
-    plt.xlabel("x value")
-    plt.ylabel(f"{function}")
-    plt.title(f"{function} Graphs at different lambda values")
-    
-    # Adding legend, which helps us recognize the curve according to it's color
+        plt.plot(x, y, color='r', label=f"t=3 ; lambda=1/3 , p={y_p}")
+        plt.plot(x, z, color='g', label=f"t=4 ; lambda=1/4 , p={z_p}")
+        plt.plot(x, a, color="b", label=f"t=5 ; lambda=1/5 , p={a_p}")
+        plt.xlabel("x value")
+        plt.ylabel(f"{function}")
+        plt.title(f"{function} Graphs at different lambda values")
+        
     plt.legend()
     plt.show()
     
-
-def integrad_eq(eq, start, end):
-    return quad(eq, start, end )[0]
-
-
 print(plotting_equation())
-
